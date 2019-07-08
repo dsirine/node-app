de911b8c588de7d5a669505f5ee6769a99099d4f
dridi-sirine <ssirine131@gmail.com>
2019-07-08 21:58:38 +0200

    test of github pull request

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 var request = require('supertest');
 var app = require('../index.js');
 describe('GET /', function() {
- it('respond with hey world', function(done) {
- //navigate to root and check the the response is "hey world"
- request(app).get('/').expect('hey world', done);
+ it('respond with Bonjour', function(done) {
+ //navigate to root and check the the response is "Bonjour"
+ request(app).get('/').expect('Bonjour', done);
  });
 });
